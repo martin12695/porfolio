469d2dc5e6ac7bd36309bd592bacc706664f253c
DESKTOP-SIIJPEP\Kanglouis <kang@adquestasia.com>
2017-08-07 15:13:26 +0700

    fix sidebar menu

--- resources/views/layouts/adlayout.blade.php
+++ resources/views/layouts/adlayout.blade.php
@@ -45,7 +45,7 @@
 
 <!--Header-part-->
 <div id="header">
-  <h1><a href="dashboard.html">admin</a></h1>
+  <h1><a href="/dashboard">admin</a></h1>
 </div>
 <!--close-Header-part--> 
 
@@ -55,26 +55,10 @@
   <ul class="nav">
     <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome User</span><b class="caret"></b></a>
       <ul class="dropdown-menu">
-        <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
-        <li class="divider"></li>
-        <li><a href="#"><i class="icon-check"></i> My Tasks</a></li>
-        <li class="divider"></li>
         <li><a href="login.html"><i class="icon-key"></i> Log Out</a></li>
       </ul>
     </li>
-    <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text">Messages</span> <span class="label label-important">5</span> <b class="caret"></b></a>
-      <ul class="dropdown-menu">
-        <li><a class="sAdd" title="" href="#"><i class="icon-plus"></i> new message</a></li>
-        <li class="divider"></li>
-        <li><a class="sInbox" title="" href="#"><i class="icon-envelope"></i> inbox</a></li>
-        <li class="divider"></li>
-        <li><a class="sOutbox" title="" href="#"><i class="icon-arrow-up"></i> outbox</a></li>
-        <li class="divider"></li>
-        <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> trash</a></li>
-      </ul>
-    </li>
-    <li class=""><a title="" href="#"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
-    <li class=""><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+    <li class=""><a target="_blank" ="" href="/"><i class="icon icon-share-alt"></i> <span class="text">Xem trang</span></a></li>
   </ul>
 </div>
 <!--close-top-Header-menu-->
@@ -85,58 +69,30 @@
   <ul>
     <li><a href="#"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
 
-    <li class="@yield('activeSidebar')"> <a href="{{url('/dashboard/project')}}"><i class="icon icon-signal"></i> <span>Project</span></a> </li>
-
-    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li>
-
-    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-
-    <li><a href="grid.html"><i class="icon icon-fullscreen"></i> <span>Full width</span></a></li>
-
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
+    <li class="submenu @yield('activeSidebar')"> <a href="#"><i class="icon icon-list-ul"></i> <span>Project</span> <span class="label label-important">2</span></a>
       <ul>
-        <li><a href="form-common.html">Basic Form</a></li>
-        <li><a href="form-validation.html">Form with Validation</a></li>
-        <li><a href="form-wizard.html">Form with Wizard</a></li>
+        <li><a href="{{url('/dashboard/project')}}">List project</a></li>
+        <li><a href="{{url('/dashboard/project/add')}}">Add new</a></li>
       </ul>
     </li>
 
-    <li><a href="buttons.html"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
-
-    <li><a href="interface.html"><i class="icon icon-pencil"></i> <span>Eelements</span></a></li>
-
-    <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span> <span class="label label-important">5</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Concept</span> <span class="label label-important">2</span></a>
       <ul>
-        <li><a href="index2.html">Dashboard2</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="calendar.html">Calendar</a></li>
-        <li><a href="invoice.html">Invoice</a></li>
-        <li><a href="chat.html">Chat option</a></li>
+        <li><a href="">List concept</a></li>
+        <li><a href="">Add new</a></li>
       </ul>
     </li>
 
-    <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span> <span class="label label-important">4</span></a>
+    <li><a href=""><i class="icon icon-tint"></i> <span>Edit profile</span></a></li>
+
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Upload images</span> <span class="label label-important">2</span></a>
       <ul>
-        <li><a href="error403.html">Error 403</a></li>
-        <li><a href="error404.html">Error 404</a></li>
-        <li><a href="error405.html">Error 405</a></li>
-        <li><a href="error500.html">Error 500</a></li>
+        <li><a href="">List Images</a></li>
+        <li><a href="">Upload new</a></li>
       </ul>
     </li>
-    <li class="content"> <span>Monthly Bandwidth Transfer</span>
-      <div class="progress progress-mini progress-danger active progress-striped">
-        <div style="width: 77%;" class="bar"></div>
-      </div>
-      <span class="percent">77%</span>
-      <div class="stat">21419.94 / 14000 MB</div>
-    </li>
-    <li class="content"> <span>Disk Space Usage</span>
-      <div class="progress progress-mini active progress-striped">
-        <div style="width: 87%;" class="bar"></div>
-      </div>
-      <span class="percent">87%</span>
-      <div class="stat">604.44 / 4000 MB</div>
-    </li>
+
+    <li><a href="#"><i class="icon icon-home"></i> <span>Contact to webmaster</span></a> </li>
   </ul>
 </div>
 <!--sidebar-menu-->
