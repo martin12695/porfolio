342a109d450f344453864836bf318668e4118955
DESKTOP-SIIJPEP\Kanglouis <kang@adquestasia.com>
2017-08-09 23:42:47 +0700

    Smoothy back to top, view image detail (@sketch), menu, mobile/tablet layouts, prevent right click

--- public/js/libs.js
+++ public/js/libs.js
@@ -3,8 +3,31 @@ var scrollPos = 0;
 window.onscroll = backToTop;
 
 $(document).ready(function(){
+	$(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+	
 	$('#backtotop').click(function(){
-		
+		$("html, body").animate({
+            scrollTop: 0
+        }, 600);
+	});
+
+	var $img = $('img');
+	$img.each(function(){
+		var $this = $(this);
+		$this.click(function(){
+			var src = $this.attr('src');
+			var Img = $('#imgDetail img');
+			var imgDetail = $('#imgDetail');
+			Img.attr('src', src);
+			imgDetail.fadeIn();
+		});
+	});
+
+	$('#imgDetailClose').click(function(){
+		var imgDetail = $('#imgDetail');
+		imgDetail.fadeOut();
 	});
 });
 
